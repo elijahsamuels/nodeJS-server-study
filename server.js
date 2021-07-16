e84c043145ebf9ea0347c99968df5846e8c1d16b
Elijah Samuels <elijahsamuels@gmail.com>
2021-07-16 11:57:13 -0600

    dirname filename

--- server.js
+++ server.js
@@ -2,14 +2,23 @@ const http = require("http");
 const HOSTNAME = process.env.HOSTNAME || "localhost";
 const PORT = process.env.PORT || 3000;
 
+const DIRNAME = __dirname
+const FILENAME = __filename
+
 // part of the global object is "process". global.process
 
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/plain");
-    response.end("hello world!");
+    response.end(`hello world!\nDirectory: ${DIRNAME}\nFilename: ${FILENAME}`);
 });
 
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server is running at http://${HOSTNAME}:${PORT}`);
+    console.log(`Directory: ${DIRNAME} Filename: ${FILENAME}`);
+	
+
 });
+
+// to run:
+// node server
