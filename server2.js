e111b7dc82c3440f7dfd9170f370f88d10f48d91
Elijah Samuels <elijahsamuels@gmail.com>
2021-07-16 13:22:11 -0600

    writing to files

--- server2.js
+++ server2.js
@@ -8,7 +8,8 @@
 //     console.log(data.toString());
 // });
 
-// OR ANOTHER WAY
+//     // OR ANOTHER WAY
+
 // const fs = require("fs");
 
 // fs.readFile("test.txt", "utf8", (err, data) => {
@@ -21,7 +22,7 @@
 
 // console.log("log from outside of the block");
 
-// a blocking method
+//     // a blocking method
 
 // const fs = require("fs");
 
@@ -33,14 +34,43 @@
 // }
 // console.log("log from outside of the block");
 
-// and a method using destructuring
+//     // and a method using destructuring
 
-const { readFile, readFileSync } = require("fs");
+// const { readFile, readFileSync } = require("fs");
 
-try {
-    const data = readFileSync("test.txt", "utf8");
-    console.log(data);
-} catch (err) {
-    console.error(err);
-}
-console.log("log from outside of the block");
+// try {
+//     const data = readFileSync("test.txt", "utf8");
+//     console.log(data);
+// } catch (err) {
+//     console.error(err);
+// }
+// console.log("log from outside of the block");
+
+//     // and a method using destructuring
+//     // default behavior of writeFile is to OVERWRITE!!! be careful..
+//     // use {flag: "a"} as the third argument in writeFile to append
+
+// const { writeFile, writeFileSync } = require("fs");
+// const newContent = "this is some new text"
+
+// writeFile('test.txt', newContent, {flag: "a"}, (err) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     else {
+//         console.log("content written ");
+//     }
+// })
+
+const { appendFile } = require("fs");
+const newContent = "\n - This is some more new text";
+
+appendFile("test.txt", newContent, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        console.log("content written ");
+    }
+});
