cc32f65ce73d66073fc50e7ac502ba4c3398ae99
Elijah Samuels <elijahsamuels@gmail.com>
2021-07-16 13:48:06 -0600

    export/import modules

---
+++ addNums.js
@@ -0,0 +1,13 @@
+// function addNums(a,b) {
+// 	return a+b
+// }
+
+// module.exports = addNums
+
+// const addNums = (a,b) => {
+// 	return a+b
+// }
+
+export const addNums = (a,b) => {
+	return a+b
+}
---
+++ server3.js
@@ -0,0 +1,13 @@
+// const addNums = require("./addNums");
+
+// const sum = addNums(2, 2);
+// console.log(sum);
+
+//      // The ES6 way:
+//      // Use import XXX from filepath
+
+
+import {addNums} from './addNums.js'
+
+const sum = addNums(2, 2);
+console.log(sum);
